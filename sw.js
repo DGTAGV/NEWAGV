49989b235df4a8c5ac8b404fa2a8a5c369a99587
DGTAGV <tokiabuji@gmail.com>
2026-08-12 22:54:43 +0900

    sw.js

--- sw.js
+++ sw.js
@@ -1,19 +1,42 @@
-const CACHE_NAME = 'agv-cache-v1';
+const CACHE_NAME = 'agv-cache-v2'; 
+
 const urlsToCache = [
   './',
   './index.html',
-  './icon.png',
-  './manifest.json'
+  './manifest.json',
+  './icon.png'
 ];
 
 self.addEventListener('install', event => {
+  self.skipWaiting();
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
   );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+     
+            return caches.delete(cacheName); 
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
   );
 });
